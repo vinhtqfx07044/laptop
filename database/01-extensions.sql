cc218cd4dc1e5268aab8441ce5f9477d233738e9
vinhtqfx07044 <vinhtqfx07044@funix.edu.vn>
2025-10-30 06:51:56 +0000

    feat(db): add immutable_unaccent function for fuzzy search

--- database/01-extensions.sql
+++ database/01-extensions.sql
@@ -7,3 +7,14 @@ CREATE EXTENSION IF NOT EXISTS unaccent;
 
 -- Verify installation
 SELECT extname FROM pg_extension WHERE extname IN ('vector', 'hstore', 'uuid-ossp', 'pg_trgm', 'unaccent');
+
+-- Create immutable_unaccent function for fuzzy search
+-- This function creates an immutable version of unaccent for use in indexes
+CREATE OR REPLACE FUNCTION immutable_unaccent(text)
+RETURNS text
+LANGUAGE sql
+IMMUTABLE
+RETURNS NULL ON NULL INPUT
+AS $$
+    SELECT unaccent($1);
+$$;
